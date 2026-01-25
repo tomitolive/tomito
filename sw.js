b0496d52f2069914fe47bb66fb02429a715770be
Your Name <you@example.com>
2026-01-25 18:23:19 +0100

    df

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10473372
+    "zoneId": 10479435
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
