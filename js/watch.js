b4ee35158f7231363dc1b665f23071ce2df5ced4
Your Name <you@example.com>
2026-01-26 06:38:55 +0100

    Save changes before switching to saber2

--- js/watch.js
+++ js/watch.js
@@ -122,7 +122,7 @@ const SERVERS = [
     },
     {
         id: 'server_9',
-        n',
+        name:'جديد',
         movieUrl: 'https://vidsrc-embed.ru/embed/movie',
         tvUrl: 'https://vidsrc-embed.ru/embed/tv',
         quality: 'FHD',
