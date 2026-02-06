e5ec43d33b61b929f7aa1967af36d238eca97e66
Ismek <email.dialek@example.com>
2026-02-07 00:06:38 +0100

    qdf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // استخدام "/" لـ Domain مخصص أو User Page، أو اسم الـ Repo إذا كان Project Page
   server: {
     port: 8080,
     host: true,
