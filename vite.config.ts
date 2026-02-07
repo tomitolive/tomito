e0a9696ef80635c67860f50c9927e62b312ca470
Your Name <you@example.com>
2026-02-07 01:52:46 +0100

    Configure custom domain tomito.xyz

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/tomito/', // Smmit l-repo dyalk f GitHub hna
+  base: '/', // Custom domain tomito.xyz
 })
